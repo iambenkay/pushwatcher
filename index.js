132591ef22d3bc493ab83850d9e5f5821f499b21
Benjamin Kayode <benjamin.node@gmail.com>
2019-07-02 10:05:03 +0100

    Fixed Tweet Issue

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post("/git-twit", (req, res) => {
     let push = req.body;
 
-    let toTweet = `***GIT ${req.headers['x-github-event'].toUpperCase()} NOTIFICATION***\n\ngit >> ${push.head_commit.message}\n\n\t\tLast commit by: ${push.head_commit.author.name}\n\t\t${push.head_commit.author.email}.\n\nThere are a total of ${push.commits.length()} commits in this push.\n\n(This stat was published by pushwatcher)`;
+    let toTweet = `***GIT ${req.headers['x-github-event'].toUpperCase()} NOTIFICATION***\n\ngit >> ${push.head_commit.message}\n\n\t\tLast commit by: ${push.head_commit.author.name}\n\t\t${push.head_commit.author.email}.\n\nThere are a total of ${push.commits.length} commits in this push.\n\n(This stat was published by pushwatcher)`;
 
     client.post('statuses/update', { status: toTweet })
         .then(tweet => {
